31b36013f97efcfedfd3b421f59bd804ac406987
Nawcodes <nawcodes@gmail.com>
2022-02-02 08:57:02 +0700

    release with app 1.0.0v

--- application/views/profile.php
+++ application/views/profile.php
@@ -7,34 +7,45 @@
         </div>
         <div class="card-body">
 
-          <?=form_open_multipart()?>
-          <?=$this->session->flashdata('message')?>
+          <?= form_open_multipart() ?>
+          <?= $this->session->flashdata('message') ?>
+          <div class="row mb-5">
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="text" value="<?= $detail->email ?>" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" disabled>
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center">
+              <img src="<?= base_url('assets/images/ktp/') . $detail->img ?>" alt="" width="" style="width:250px; !important">
+            </div>
+          </div>
 
-        <div class="form-group">
-          <label for="exampleInputEmail1">Nama Lengkap</label>
-          <input type="text" value="<?=$detail->nama?>" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama Lengkap">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Alamat</label>
-          <textarea name="alamat" class="form-control" placeholder="Masukan Alamat"><?=$detail->alamat?></textarea>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Nomor HP</label>
-          <input type="text" name="no_hp" value="<?=$detail->no_hp?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nomor HP">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Username</label>
-          <input type="text" name="username" value="<?=$detail->username?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Username">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Password</label>
-          <input type="Password" name="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Kosongkan jika tidak ingin mengganti">
-        </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nama Lengkap</label>
+            <input type="text" value="<?= $detail->nama ?>" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama Lengkap">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Alamat</label>
+            <textarea name="alamat" class="form-control" placeholder="Masukan Alamat"><?= $detail->alamat ?></textarea>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nomor HP</label>
+            <input type="text" name="no_hp" value="<?= $detail->no_hp ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nomor HP">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" name="username" value="<?= $detail->username ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Username">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Password</label>
+            <input type="Password" name="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Kosongkan jika tidak ingin mengganti">
+          </div>
 
           <button type="submit" class="btn btn-primary">Simpan</button>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </div>
